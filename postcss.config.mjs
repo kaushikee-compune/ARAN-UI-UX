3b891006cd676c45377d2e7fb9edaf2a3d0ab8ec
kaushikee-compune <kaushikee@compune.com>
2025-10-31 06:12:02 +0530

    postcssconfig

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {},  // ✅ correct syntax
+  },
 };
 
 export default config;
